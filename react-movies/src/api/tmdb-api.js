cf8b6623d4d11b3df7ed8a71d3d4a46c0702fc13
jerca <20101456@mail.wit.ie>
2025-01-06 20:15:23 +0000

    Integrated Users Endpoints

--- react-movies/src/api/tmdb-api.js
+++ react-movies/src/api/tmdb-api.js
@@ -154,3 +154,26 @@ export const getMovieRecommendations = ({ queryKey }) => {
  });
 };
 
+export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+
